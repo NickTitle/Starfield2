535d90289b9fcca68d1bfef50e16a103eec8ed5d
Nicholas Esposito <nickespo8@gmail.com>
2014-03-20 16:19:11 -0400

    added beginning particles to ship

---
+++ Assets/scripts/ParticleSortingLayer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleSortingLayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		particleSystem.renderer.sortingLayerName = "background";
+		particleSystem.renderer.sortingOrder = 2;
+	}
+}
